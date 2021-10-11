5e32a4225f73f90971ac9a8f7561a13daca11659
akshay <akshay.patil@successive.tech>
2021-10-11 13:19:00 +0530

    Getting started with react(component and Tools)

---
+++ src/Component/TextField/style.js
@@ -0,0 +1,43 @@
+export const error = {
+  color: 'red',
+  'margin-left': '5px',
+};
+
+export const errorBorder = {
+  border: '1px solid #f00',
+  width: '100%',
+  'margin-left': '5px',
+  padding: '12px 20px',
+  display: 'inline-block',
+  'border-radius': '4px',
+  'box-sizing': 'border-box',
+};
+
+export const container = {
+  width: '100%',
+  display: 'flex',
+  'flex-direction': 'column',
+  border: '1px solid',
+};
+
+export const inputContainer = {
+  width: '100%',
+  display: 'flex',
+  'flex-direction': 'column',
+  border: '1px solid',
+  'margin-left': '5px',
+};
+
+export const input = {
+  width: '100%',
+  'margin-left': '5px',
+  padding: '12px 20px',
+  display: 'inline-block',
+  border: '1px solid #ccc',
+  'border-radius': '4px',
+  'box-sizing': 'border-box',
+};
+
+export const headingStyle = {
+  'margin-left': '5px',
+};
