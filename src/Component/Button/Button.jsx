32b3a339c1faa8b698365b603f4f617b9a7b733e
akshay <akshay.patil@successive.tech>
2021-10-18 16:38:47 +0530

    remaining

---
+++ src/Component/Button/Button.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Button = (props) => {
+//   const {
+//     color,
+//     disabled,
+//     style,
+//     value,
+//     onclick,
+//   } = props;
+
+//   return (
+//     <>
+//       <div value={value}>
+//           {value
+//           ?(
+//               <b>
+//                   submit
+//               </b>
+//           ):''}
+//         <input onClick={onclick} color= ? type="button" value={value} disabled={false} />
+//       </div>
+//     </>
+//   );
+// };
